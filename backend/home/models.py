98d0b40ae1c28f14a42c89fa3f4c7ec6fb68c474
crowdbotics-apps <team@crowdbotics.com>
2023-06-29 19:04:45 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -4,4 +4,4 @@ class Hello(models.Model):
     'Generated Model'
     ffqegfgsd = models.BigIntegerField(blank=True,)
     lkkkdffssdkj = models.CharField(null=True,blank=True,max_length=256,)
-    ksdjjd = models.DateTimeField(null=True,blank=True,auto_now=True,)
+    ksdjjdkhj = models.DateTimeField(null=True,blank=True,auto_now=True,)
